ff05e451e0cb13492557c31c5d9354f648227bec
_CHAI__ <apichai.freedom@gmail.com>
2020-06-18 07:14:55 +0700

    remove all semicolons.

--- src/services/api/graphql/product.api.js
+++ src/services/api/graphql/product.api.js
@@ -1,4 +1,4 @@
-import { gql } from 'apollo-boost';
+import { gql } from 'apollo-boost'
 import httpClient from './apollo.client'
 
 export const getProducts = () => httpClient.product.query({
--- src/services/api/graphql/user.api.js
+++ src/services/api/graphql/user.api.js
@@ -1,4 +1,4 @@
-import { gql } from 'apollo-boost';
+import { gql } from 'apollo-boost'
 import httpClient from './apollo.client'
 
 export const getUsers = () => httpClient.user.query({
