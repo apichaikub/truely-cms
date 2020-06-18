54ff8c09323a341d7db17e0515ecf7832335828c
_CHAI__ <apichai.freedom@gmail.com>
2020-06-18 08:38:36 +0700

    fix conflict.

--- src/helpers/graphql/index.js
+++ src/helpers/graphql/index.js
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
-import storage from '../../storages'
-
-export const request = (operation) => {
-  const accessToken = storage.getAccessToken()
-
-  operation.setContext({
-    headers: {
-      authorization: accessToken ? `Bearer ${accessToken}` : ''
-    }
-  })
-}
-
-export const onError = ({ graphQLErrors, networkError }) => {
-  console.log('graphQLErrors', graphQLErrors)
-=======
 import request from './request'
 import onError from './onError'
 
 export {
   request,
   onError
->>>>>>> parent of 6430fee... refactor : helper graphql.
 }
