6430feee6376b4e7ba85da4dba3a3ffcc3885cf7
_CHAI__ <apichai.freedom@gmail.com>
2020-06-18 07:55:46 +0700

    refactor : helper graphql.

--- src/helpers/graphql/index.js
+++ src/helpers/graphql/index.js
@@ -1,7 +1,13 @@
-import request from './request'
-import onError from './onError'
+export const request = (operation) => {
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ5ODczYmIzLTMyODgtNDk4Zi1hZWVmLTgxYjBmNGIyZDkyMCIsInJvbGUiOiJBRE1JTiIsInNjb3BlcyI6WyJwcm9kdWN0LnJlYWQiLCJwcm9kdWN0LndyaXRlIiwidXNlci5yZWFkIiwidXNlci53cml0ZSJdLCJpYXQiOjE1OTIzMTM5MzMsImV4cCI6MTU5MjMxNDgzM30.aGx8e2HRT8r47HUHfIAlJDWzf55TD1Ebnk9SuE2sEII'
 
-export {
-  request,
-  onError
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  })
+}
+
+export const onError = ({ graphQLErrors, networkError }) => {
+  console.log('graphQLErrors', graphQLErrors)
 }
