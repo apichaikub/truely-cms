52976325386f79959811c025f3237e403550b8e3
_CHAI__ <apichai.freedom@gmail.com>
2020-06-25 08:22:11 +0700

    refactor : change files name to Routers.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Routers from './Routers'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routers />
   </React.StrictMode>,
   document.getElementById('root')
 )
