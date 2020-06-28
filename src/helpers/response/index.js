8fee6bba7db1f00fdd083f07ee9d195411aea18c
_CHAI__ <apichai.freedom@gmail.com>
2020-06-28 16:48:49 +0700

    add interceptor for axios to call rest api, set tokens to storages.

--- src/helpers/response/index.js
+++ src/helpers/response/index.js
@@ -1,5 +1,9 @@
 import getFinalResponse from './getFinalResponse'
+import interceptorSuccess from './interceptorSuccess'
+import interceptorError from './interceptorError'
 
 export {
   getFinalResponse,
+  interceptorSuccess,
+  interceptorError,
 }
--- src/services/api/rest/axios.client.js
+++ src/services/api/rest/axios.client.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
+import { interceptorSuccess as responseSuccess, interceptorError as responseError } from '../../../helpers/response'
 
-export default axios.create({
+const client = axios.create({
   baseURL: process.env.REACT_APP_API,
 })
+
+client.interceptors.response.use(responseSuccess, responseError);
+
+export default client
