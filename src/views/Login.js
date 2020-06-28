7027e2b2247c3afef8b7ced20384fd4b8ae0e94e
_CHAI__ <apichai.freedom@gmail.com>
2020-06-28 16:52:07 +0700

    test this script on production.

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { authActions, meActions } from '../store/actions'
 
-const Login = () => {
+const Login = (props) => {
+  const dispatch = useDispatch()
+  const auth = useSelector(state => state.auth)
+  const me = useSelector(state => state.me)
+
+  useEffect(() => {
+    dispatch(meActions.getMe())
+  }, [dispatch]);
+
   return (
     <div className="App">
       Login Page
+      <button onClick={() => dispatch(authActions.getTokens({ username: 'admin1', password: '123456' }))}>
+        Click
+      </button>
+      <pre>{JSON.stringify(auth)}</pre>
+      <pre>{JSON.stringify(me)}</pre>
     </div>
   )
 }
