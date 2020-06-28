4169c918b4936f0edc0073f0bce767d0c03e16fb
_CHAI__ <apichai.freedom@gmail.com>
2020-06-28 20:35:27 +0700

    test authentication on production.

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { authActions, meActions } from '../store/actions'
 
@@ -6,20 +7,26 @@ const Login = (props) => {
   const dispatch = useDispatch()
   const auth = useSelector(state => state.auth)
   const me = useSelector(state => state.me)
-
-  useEffect(() => {
-    dispatch(meActions.getMe())
-  }, [dispatch])
-
+  
   return (
     <div className="App">
       Login Page
+      <br/>
+      <br/>
       <button onClick={() => dispatch(authActions.getTokens({ username: 'admin1', password: '123456' }))}>
-        Click
+        Get Tokens
       </button>
+      <br/>
+      <br/>
+      <button onClick={() => dispatch(meActions.getMe())}>
+        Get Me
+      </button>
+      <br/>
       <pre>{JSON.stringify(auth)}</pre>
       <pre>{JSON.stringify(me)}</pre>
       <pre>{JSON.stringify(process.env)}</pre>
+      <Link to="/products">Products</Link> <br/>
+      <Link to="/users">Users</Link>
     </div>
   )
 }
