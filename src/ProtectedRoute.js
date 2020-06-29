85ec801c29cd199997db255077d2bdb905e69795
_CHAI__ <apichai.freedom@gmail.com>
2020-06-29 08:17:58 +0700

    refactor : check user roles, clean up code.

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { Route, Redirect } from 'react-router-dom'
 import { ROUTER_PATH } from './enum'
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
+const ProtectedRoute = ({ component: Component, roles, ...rest }) => {
   const { isLoggedIn } = useSelector(state => state.auth)
+  const meState = useSelector(state => state.me)
 
   if(!isLoggedIn) {
+    return <Redirect to={ROUTER_PATH.ENUM.LOGIN} />
+  }
+
+  if(!roles.includes(meState.role)) {
     return <Redirect to={ROUTER_PATH.ENUM.LOGIN} />
   }
 
--- src/Routers.js
+++ src/Routers.js
@@ -1,28 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import { ROUTER_PATH } from './enum'
+import { ROUTER_PATH, ROLE_USER } from './enum'
 import ProtectedRoute from './ProtectedRoute'
 import Login from './views/Login'
 import Products from './views/Products'
 import Users from './views/Users'
 
-const {
-  ENUM: {
-    HOME,
-    LOGIN,
-    PRODUCTS,
-    USERS,
-  },
-} = ROUTER_PATH
+const { ENUM: ROUTER } = ROUTER_PATH
+const { ENUM: ROLE } = ROLE_USER
 
 const Routers = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path={HOME} component={Login} />
-        <Route exact path={LOGIN} component={Login} />
-        <ProtectedRoute exact path={PRODUCTS} component={Products} />
-        <ProtectedRoute exact path={USERS} component={Users} />
+        <Route exact path={ROUTER.HOME} component={Login} />
+        <Route exact path={ROUTER.LOGIN} component={Login} />
+        <ProtectedRoute exact path={ROUTER.PRODUCTS} component={Products} roles={[ROLE.ADMIN]} />
+        <ProtectedRoute exact path={ROUTER.USERS} component={Users} roles={[ROLE.ADMIN]}/>
       </Switch>
     </Router>
   )
