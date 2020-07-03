8f82ff1a9a0c482ee085a2ccb17c3ce5b548c5ad
_CHAI__ <apichai.freedom@gmail.com>
2020-07-03 08:09:19 +0700

    feature : login pages.

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,32 +1,97 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import { authActions, meActions } from '../store/actions'
+import { useForm, Controller } from 'react-hook-form'
+import { CButton, CCard, CCardBody, CCardGroup, CCol, CContainer, CForm, CInput, CInputGroup, CInputGroupPrepend, CInputGroupText, CRow, CFormText, CAlert } from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { authActions } from '../store/actions'
+import { ROUTER_PATH } from '../enum'
 
-const Login = (props) => {
+const Login = () => {
   const dispatch = useDispatch()
-  const auth = useSelector(state => state.auth)
-  const me = useSelector(state => state.me)
-  
+  const authState = useSelector(state => state.auth)
+  const { control, errors, handleSubmit } = useForm()
+
+  // handler function
+  const onSubmit = ({ username, password }, e) => {
+    dispatch(authActions.requestGetTokens({username, password}))
+  }
+
+  // if user is already logged in, then redirect to product pages
+  if(authState.loggedin) {
+    return <Redirect to={ROUTER_PATH.ENUM.PRODUCTS} />
+  }
+
   return (
-    <div className="App">
-      Login Page
-      <br/>
-      <br/>
-      <button onClick={() => dispatch(authActions.requestGetTokens({ username: 'admin1', password: '123456' }))}>
-        Get Tokens
-      </button>
-      <br/>
-      <br/>
-      <button onClick={() => dispatch(meActions.requestGetMe())}>
-        Get Me
-      </button>
-      <br/>
-      <pre>{JSON.stringify(auth)}</pre>
-      <pre>{JSON.stringify(me)}</pre>
-      <pre>{JSON.stringify(process.env)}</pre>
-      <Link to="/products">Products</Link> <br/>
-      <Link to="/users">Users</Link>
+    <div className="c-app c-default-layout flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md="8">
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm onSubmit={handleSubmit(onSubmit)}>
+                    <h1>Truely - CMS</h1>
+                    <p className="text-muted">if you don't have an account please contact an admin.</p>
+                    { authState.error && (
+                      <CAlert className="py-3 mt-2" color="danger">
+                        Username or password is incorrect.
+                      </CAlert>
+                    )}
+                    <CInputGroup className="mb-3">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-user" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <Controller
+                        type="text"
+                        placeholder="Username"
+                        as={CInput}
+                        name="username"
+                        control={control}
+                        defaultValue="admin1"
+                        rules={{ required: true }}
+                      />
+                      { errors.username && (
+                        <CInputGroup>
+                          <CFormText className="help-block">Please enter your username</CFormText>
+                        </CInputGroup>
+                      )}
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-lock-locked" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <Controller
+                        type="password"
+                        placeholder="Password"
+                        as={CInput}
+                        name="password"
+                        control={control}
+                        defaultValue="123456"
+                        rules={{ required: true }}
+                      />
+                      { errors.password && (
+                        <CInputGroup>
+                          <CFormText className="help-block">Please enter your password</CFormText>
+                        </CInputGroup>
+                      )}
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs="12">
+                        <CButton type="submit" color="success" className="px-4 col-12">Login</CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
     </div>
   )
 }
