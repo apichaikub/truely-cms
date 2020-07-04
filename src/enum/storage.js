f26d64c9667dbca31e512b78a9fc4aece2793787
_CHAI__ <apichai.freedom@gmail.com>
2020-07-04 16:46:39 +0700

    add value to storage enum,.

--- src/enum/storage.js
+++ src/enum/storage.js
@@ -1,7 +1,9 @@
 export const STORAGE_NAME = Object.freeze({
   ENUM: {
     ACCESS_TOKEN: 'accessToken',
-    REFRESH_TOKEN: 'refreshToken'
+    REFRESH_TOKEN: 'refreshToken',
+    SIDEBAR_SHOW: "sidebarShow",
+    SIDEBAR_MINIMIZE: "sidebarMinimize",
   },
   get values() {
     return Object.values(this.ENUM)
