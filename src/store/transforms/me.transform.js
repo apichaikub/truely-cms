12cd262b8b061a18885b3fb2bc1605ad1463ad8a
_CHAI__ <apichai.freedom@gmail.com>
2020-07-04 17:23:47 +0700

    refactor : fix to 2 spaces.

--- src/store/transforms/me.transform.js
+++ src/store/transforms/me.transform.js
@@ -1,16 +1,16 @@
 const meTransforms = {
-    /**
-     * @params { Object } payload
-     * @returns { Object }
-     * @returns { String, UUID } userId
-     * @returns { String } username
-     * @returns { String, ENUM } role : see more /enum/role.js
-     */
-    info: (payload = {}) => ({
-        userId: payload.userId,
-        username: payload.username,
-        role: payload.role,
-    })
+  /**
+    * @params { Object } payload
+    * @returns { Object }
+    * @returns { String, UUID } userId
+    * @returns { String } username
+    * @returns { String, ENUM } role : see more /enum/role.js
+    */
+  info: (payload = {}) => ({
+    userId: payload.userId,
+    username: payload.username,
+    role: payload.role,
+  })
 }
 
 export default meTransforms
