c5a7383aa5301c8fc126e19cee5fb1552c271095
_CHAI__ <apichai.freedom@gmail.com>
2020-07-05 14:40:47 +0700

    update enum, added comment.

--- src/enum/response.js
+++ src/enum/response.js
@@ -1,7 +1,11 @@
 export const RESPONSE_KEY = Object.freeze({
     ENUM: {
+      // Rest
       RESULTS: "results",
+      // GraphQL
       DATA: "data",
+      ME: "me",
+      PRODUCTS: "products",
     },
     get values() {
       return Object.values(this.ENUM)
