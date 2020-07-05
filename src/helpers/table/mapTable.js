90ecd00c773c8c3798b33f1d54be523073ccbd7a
_CHAI__ <apichai.freedom@gmail.com>
2020-07-05 21:49:53 +0700

    add comment for `mapTable` functions.

--- src/helpers/table/mapTable.js
+++ src/helpers/table/mapTable.js
@@ -1,4 +1,36 @@
-const mapTable = (mappingValue, mappingPattern) => {
+/**
+ * this function use for mapping state, or transform them to table data.
+ * @param {*} mappingValue value that we want to transform.
+ * @param {*} mappingPattern pattern that we want to mapping with value.
+ * @returns {Array} table data
+ 
+  For example
+
+  mappingValue:
+  data from state in redux store, or whatever
+  [{
+    productId: '1',
+    productName: 'Product A',
+    createdAt: '2020-01-15T13:25:46.999Z',
+  }]
+
+  mappingPattern:
+  left side is state keys, right side is table fields keys
+  {
+    productId: 'id',
+    productName: 'name',
+    createdAt: 'created',
+  }
+
+  return:
+  it just like swapping between state keys and field keys
+  [{
+    id: '1',
+    name: 'Product A',
+    created: '2020-01-15T13:25:46.999Z',
+  }]
+****/
+const mapTable = (mappingValue = [], mappingPattern = {}) => {
   const generator = (item) => {
     return Object.keys(mappingPattern).reduce((acc, cur) => {
       acc[mappingPattern[cur]] = item[cur]
