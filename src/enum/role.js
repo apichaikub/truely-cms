8eaa3f6caec3ec75e7ebf0feaf51864f77838474
_CHAI__ <apichai.freedom@gmail.com>
2020-07-06 08:17:21 +0700

    clean up.

--- src/enum/role.js
+++ src/enum/role.js
@@ -1,9 +1,9 @@
 export const ROLE_USER = Object.freeze({
-    ENUM: {
-      ADMIN: "ADMIN",
-      GUEST: "GUEST",
-    },
-    get values() {
-      return Object.values(this.ENUM)
-    },
-  })
+  ENUM: {
+    ADMIN: "ADMIN",
+    GUEST: "GUEST",
+  },
+  get values() {
+    return Object.values(this.ENUM)
+  },
+})
